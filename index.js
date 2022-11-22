d1151875928d71a2a42ca17fe73fb99e66ccdc49
RoxanneGutierrez <roxannenguyenn@gmail.com>
2022-11-21 21:07:45 -0600

    commit tree a75fdb86bb73f42671698399fc2d1be6885f0fe5

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+
+const submitData = (userName, userEmail) => {
+  const body = { name: userName, email: userEmail };
+
+  const configurationObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(body),
+  };
+
+  return fetch("http://localhost:3000/users", configurationObj)
+    .then((response) => {
+      return response.json();
+    })
+    .then((newObject) => {
+      const usersList = document.querySelector("#users");
+      const newChild = document.createElement("li");
+      newChild.textContent = newObject.id;
+      usersList.appendChild(newChild);
+    })
+    .catch((error) => {
+      document.querySelector("div#error-message").textContent =
+        error["message"];
+    });
+};
